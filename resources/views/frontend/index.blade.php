da348d24b723399151f5e11398c07201f0e60e58
Aquino Tegar <aquinotgr@Aquinos-MacBook-Air.local>
2019-06-13 20:59:24 +0700

    home banner data type bug fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,16 +3,16 @@
 @section('heading')
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner" role="listbox">
-        @if (count($banners) >= 1)
-        @foreach ($banners as $key => $banner)
-            <!-- Slide One - Set the background image for this slide in the line below -->
-            <div class="carousel-item {{ $key == 0 ? 'active':'' }}" style="background-image: url('http://fcwallpaper.com/wp-content/uploads/2018/07/Wallpaper-Desktop-CR7-Juventus-HD.jpg')">>
-                <div class="carousel-caption d-none d-md-block">
-                    <h3 class="display-4">{{$banner->title}}</h3>
-                    <a class="btn btn-primary" href="{{ route('blog.post', $banner->url) }}">SELENGKAPNYA</a>
+        @if (count((array) $banners) >= 1)
+            @foreach ($banners as $key => $banner)
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item {{ $key == 0 ? 'active':'' }}" style="background-image: url('http://fcwallpaper.com/wp-content/uploads/2018/07/Wallpaper-Desktop-CR7-Juventus-HD.jpg')">>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3 class="display-4">{{$banner->title}}</h3>
+                        <a class="btn btn-primary" href="{{ route('blog.post', $banner->url) }}">SELENGKAPNYA</a>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
         @endif
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
