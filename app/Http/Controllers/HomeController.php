0e1fee2ef62b21edd4866529831ba96621ac160c
Aquino Tegar <aquinotgr@Aquinos-MacBook-Air.local>
2019-06-13 21:36:36 +0700

    homepage banners variable dump debug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,6 +31,7 @@ class HomeController extends Controller
 
         $banners = $this->client->get('api/banners/banner/home/3');
         $banners = json_decode($banners->getBody(), true);
+        dd($banners);
 
         $popularPosts = $this->client->get('api/blogs/post/category/bola/5');
         $popularPosts = json_decode($popularPosts->getBody());
