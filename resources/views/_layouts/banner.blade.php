8b2fd4cb89bc5ec397d12283a62bfb49be458ae3
Rizky Rahardian Akbar <rahardian.rizky@gmail.com>
2019-06-15 17:51:44 +0700

    update banner

--- resources/views/_layouts/banner.blade.php
+++ resources/views/_layouts/banner.blade.php
@@ -3,7 +3,7 @@
         @if (count($banners) >= 1)
             @foreach ($banners as $key => $banner)
                 <!-- Slide One - Set the background image for this slide in the line below -->
-                <div class="carousel-item {{ $key == 0 ? 'active':'' }}" style="background-image: url('http://fcwallpaper.com/wp-content/uploads/2018/07/Wallpaper-Desktop-CR7-Juventus-HD.jpg')">>
+                <div class="carousel-item {{ $key == 0 ? 'active':'' }}" style="background-image: url('{{$banner['image']}}')">>
                     <div class="carousel-caption d-none d-md-block">
                         <h3 class="display-4">{{$banner['title']}}</h3>
                         <a class="btn btn-primary" href="{{ route('blog.post', $banner['url']) }}">SELENGKAPNYA</a>
