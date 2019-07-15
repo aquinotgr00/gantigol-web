9256a7a3d0279b0bf17b5c2542ae4a394b06639e
Rizky Rahardian Akbar <rahardian.rizky@gmail.com>
2019-07-15 12:00:06 +0700

    update detail history

--- resources/views/frontend/user.blade.php
+++ resources/views/frontend/user.blade.php
@@ -206,7 +206,7 @@
                             @foreach ($order->orders as $item)
                                 <tr class="collapse" id="preorder-{{$order->id}}">
                                     <td class="expand-table align-bottom">
-                                        <img src="{{ $item->product_variant->image }}">
+                                        <img src="{{ $item->product_variant->product->image }}">
                                         <span class="ml-3 mt-4 float-left">{{$item->product_variant->name}} </span>
                                     </td>
                                     <td class="expand-table">SIZE:{{$item->model}}</td>
